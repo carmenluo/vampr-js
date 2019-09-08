581420ebfcf96162a5e0b1138e0cc174975edeec
Carmen <sdluojm@gmail.com>
2019-09-08 20:27:47 +0000

    totalDescendent and allMillennialVampires

--- vampire.js
+++ vampire.js
@@ -64,18 +64,39 @@ class Vampire {
           }
         }
       }
-     }
-     return null; 
+    }
+    return null;
   }
 
   // Returns the total number of vampires that exist
   get totalDescendents() {
-
+    let result = 0;
+    for (let vampire of this.offspring) {
+      if (vampire.offspring.length === 0) {
+        result = 1;
+      }
+      else {
+        result += vampire.totalDescendents + 1;
+      }
+    }
+    return result;
   }
 
   // Returns an array of all the vampires that were converted after 1980
   get allMillennialVampires() {
-
+    let result = [];
+    if (this.yearConverted > 1980) {
+      result.push(this);
+    }
+    if (!this.offspring) {
+      return result;
+    }
+    for (let vampire of this.offspring) {
+      let temp = vampire.allMillennialVampires;
+      temp.forEach(element => { result.push(element)        
+      });
+    }
+    return result;
   }
 
   /** Tree traversal methods **/
@@ -109,23 +130,28 @@ let rootVampire;
 
 rootVampire = new Vampire("root");
 
-let offspring1, offspring2, offspring3, offspring4, offspring5;
 
-offspring1 = new Vampire("andrew");
-offspring2 = new Vampire("sarah");
-offspring3 = new Vampire("c");
-offspring4 = new Vampire("d");
-offspring5 = new Vampire("e");
+let offspring1, offspring2, offspring3, offspring4, offspring5, offspring6, offspring7, offspring8;
+offspring1 = new Vampire("a", 1000);
+offspring2 = new Vampire("b", 900);
+offspring3 = new Vampire("c", 1400);
+offspring4 = new Vampire("d", 1890);
+offspring5 = new Vampire("e", 1990);
+offspring6 = new Vampire("f", 2000);
+offspring7 = new Vampire("g", 2010);
+offspring8 = new Vampire("h", 2017);
+
 rootVampire.addOffspring(offspring1);
-offspring1.addOffspring(offspring2);
+rootVampire.addOffspring(offspring2);
 rootVampire.addOffspring(offspring3);
 offspring3.addOffspring(offspring4);
-offspring4.addOffspring(offspring5);
- //console.log(rootVampire.vampireWithName(offspring1.name).name);
- //console.log(rootVampire.vampireWithName(rootVampire.name).name);
- //console.log(rootVampire.vampireWithName(offspring2.name).name);
- //console.log(offspring3.vampireWithName(offspring5.name).name);
-//console.log(rootVampire.vampireWithName(offspring5.name).name);
-console.log(rootVampire.vampireWithName(""));
+offspring3.addOffspring(offspring5);
+offspring5.addOffspring(offspring6);
+offspring6.addOffspring(offspring7);
+offspring2.addOffspring(offspring8);
+console.log(rootVampire.allMillennialVampires);
+// console.log(offspring1.totalDescendents);
+// console.log(offspring2.totalDescendents)
+// console.log(offspring3.totalDescendents)
 module.exports = Vampire;
 
